9dc753ed82526a4a94b133d1c07192ec6cfabc77
Mario Henrique <dark_mh@hotmail.com>
2024-02-08 07:45:40 -0300

    colocação dos cards de ações e fiis na página inicial.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home(){
-  return <h1>Home</h1>
+import {Card} from './components/Card';
+
+const acoes = ['BBAS3','BMGB4','TRPL4','NEOE3', 'CSMG3'];
+const fiis = ['MXRF11', 'HCTR11', 'KNCR11', 'BCFF11', 'DEVA11', 'VGHF11', 'BTCI11', 'MCHF11'];
+
+export default async function Home(){
+    let count = 0
+    const totalAcoes = acoes.length
+    const totalFiis = fiis.length
+    
+    return (
+      <div>
+      <h1>Monitor de Ativos</h1>
+      <div id="cardContainer">
+        {acoes.map((item, index) =>(
+          <Card key={index} code = {item} type= 'act' cotacao= 'String' PL= 'String' PVP = 'String' DY = 'String' TipoPagamento = 'String' DataCom = 'String' DataEx = 'String' Valor = 'String' time = 'String' />
+        ))}
+        {fiis.map((item, index) =>(
+          <Card key={index} code = {item} type= 'fii' cotacao= 'String' PL= 'String' PVP = 'String' DY = 'String' TipoPagamento = 'String' DataCom = 'String' DataEx = 'String' Valor = 'String' time = 'String' />
+        ))}
+      </div>
+    </div>
+  )
 }
---
+++ frontend/src/types/types.tsx
@@ -0,0 +1,13 @@
+type Ativo = {
+    code: String,
+    type: String,
+    cotacao: String,
+    PL: String,
+    PVP: String,
+    DY: String,
+    TipoPagamento: String,
+    DataCom: String,
+    DataEx: String,
+    Valor: String,
+    time: String
+}
